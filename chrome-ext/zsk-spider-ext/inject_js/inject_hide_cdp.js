5cc4c3339030c8b43499477ce7d67a3e19f4e24c
zsk <zsk@zsk>
2025-07-11 11:25:26 +0800

    cdp 隐藏加强

--- chrome-ext/zsk-spider-ext/inject_js/inject_hide_cdp.js
+++ chrome-ext/zsk-spider-ext/inject_js/inject_hide_cdp.js
@@ -8,13 +8,32 @@ delete window.chrome;
 delete window.domAutomation;
 delete window.domAutomationController;
 
-// 7. 覆盖console.debug方法（某些网站通过console.debug检测）
-const originalConsoleDebug = console.debug;
-console.debug = function () {
-    if (arguments[0] && arguments[0].includes('WebDriver')) {
-        return;
+// 3. 覆盖console.debug方法（某些网站通过console.debug检测）
+const nativeToString = Function.prototype.toString;
+const toStringMap = new WeakMap();
+
+// 4. 劫持 toString 原型
+Function.prototype.toString = new Proxy(nativeToString, {
+    apply(target, thisArg) {
+        // 如果是伪造过的函数，则返回伪造内容
+        if (toStringMap.has(thisArg)) {
+            return toStringMap.get(thisArg);
+        }
+        // 否则返回原始 toString
+        return Reflect.apply(target, thisArg, []);
     }
-    originalConsoleDebug.apply(console, arguments);
-};
+});
+// 修改 console.debug
+const originalDebug = console.debug;
+function fakeDebug(...args) {
+    if (args[0] && typeof args[0] === 'string' && args[0].includes('WebDriver')) {
+        return '123';
+    }
+    return originalDebug.apply(this, args);
+}
+// 设置伪造 toString 内容
+toStringMap.set(fakeDebug, 'function debug() { [native code] }');
+// 替换 console.debug
+console.debug = fakeDebug;
 
 console.log('CDP指纹已隐藏', window.location.href)
